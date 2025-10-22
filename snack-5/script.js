30bf5ea27a73f94c04bbccb1145d51cc229893c8
FedericoCanale <icocanale@gmail.com>
2025-10-23 01:14:06 +0200

    add filter to create a new arry with only even numbers

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,8 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+// Uso filter per creare un nuovo array con solo i numeri pari (resto della divisione per 2 uguale a 0)
+const evenNumbers = nums.filter(num => num % 2 === 0);
+
+console.log(evenNumbers);
