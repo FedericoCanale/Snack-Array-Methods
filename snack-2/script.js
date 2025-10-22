c8121868e85c3cdb91d66f1cf4c1744d9c275000
FedericoCanale <icocanale@gmail.com>
2025-10-23 01:04:21 +0200

    ex 2: add for each person to print only the name

--- snack-2/script.js
+++ snack-2/script.js
@@ -6,3 +6,6 @@ const people = [
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+// Uso forEach per iterare l'array di oggetti e stampare la proprietà name di ciascuno
+people.forEach(person => console.log(person.name))
