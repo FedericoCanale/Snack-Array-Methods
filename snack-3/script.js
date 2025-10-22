de286872bd5cbfeb3017e1dbff993137ac363b85
FedericoCanale <icocanale@gmail.com>
2025-10-23 01:07:29 +0200

    ex 3: add map to increase numbers by one

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,8 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+// Uso map per creare un nuovo array con ogni numero incrementato di 1
+const incrementedNumbers = numbers.map(num => num + 1);
+
+console.log(incrementedNumbers);
