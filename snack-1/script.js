b28379c383ae7c98eafbb9363f524b34808102ba
FedericoCanale <icocanale@gmail.com>
2025-10-23 00:57:20 +0200

    Ex 1: add for each to print all name

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,5 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+// Uso forEach per iterare l'array e stampare ogni nome
+names.forEach(name => console.log(name));
