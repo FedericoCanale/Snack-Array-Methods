ec9ec892a9b763900d3782791f31f64b1623810c
FedericoCanale <icocanale@gmail.com>
2025-10-23 01:20:56 +0200

    ex 7: use find to search first element with the right id

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,8 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+// Uso find per trovare il primo elemento che soddisfa la condizione (id === 2)
+const student = students.find(student => student.id === 2);
+
+console.log(student);
